58a4559fcf88105a59459b0682c21ff2e3d16ef9
Faraz Baloch <faraz.ahmad0366@gmail>
2025-04-27 19:38:58 +0500

    adding buttons

--- login_ui/lib/pages/splash_page.dart
+++ login_ui/lib/pages/splash_page.dart
@@ -10,13 +10,89 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _buildSplashScreen());
+    return Scaffold(body: _buildSplashScreen(context));
   }
 
-  Widget _buildSplashScreen() {
+  Widget _buildSplashScreen(BuildContext context) {
+    return Stack(
+      children: [_background(context), _logo(context)],
+    );
+  }
+
+  Widget _background(BuildContext context) {
     return Container(
-      color: Colors.blue,
-      child: Center(child: Text('Splash Screen')),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            const Color.fromARGB(255, 7, 25, 122),
+            const Color.fromARGB(255, 149, 196, 224),
+          ],
+          stops: [0.38, 0.4],
+        ),
+      ),
+    );
+  }
+
+  Widget _logo(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 40.0,
+                left: 40.0,
+                bottom: 20.0,
+                right: 40.0,
+              ),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/welcome.png',
+                    height: 300,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 20.0,
+                left: 30.0,
+                right: 30.0,
+              ),
+              height: 400,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(
+                    "Find the students that fits your knowledge and interests",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
